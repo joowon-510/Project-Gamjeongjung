19e324b14c353c244878d6e3ccc5c2be7f2a4dcf
SOOYON <pksuyon1122@naver.com>
2025-04-17 08:58:43 +0900

    init

---
+++ src/custom.d.ts
@@ -0,0 +1,10 @@
+// src/custom.d.ts 생성
+
+declare module "*.svg" {
+  import * as React from "react";
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+  const src: string;
+  export default src;
+}
