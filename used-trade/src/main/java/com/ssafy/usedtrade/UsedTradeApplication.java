d7ade5cdee06df52be7e34f3af5a80e9527eabd8
Ruth <v12rhtjdxo@naver.com>
2025-04-16 10:10:01 +0900

    Chore: 나머지 파일 추가

---
+++ used-trade/src/main/java/com/ssafy/usedtrade/UsedTradeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.usedtrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedTradeApplication.class, args);
+	}
+
+}
