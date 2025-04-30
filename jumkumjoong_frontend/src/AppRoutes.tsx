7827ff76a2031d80d71a6024ff027311bfaa1f36
SOOYON <pksuyon1122@naver.com>
2025-04-30 09:05:20 +0900

    Feat: 리뷰 페이지 구현
    - api 연결 미구현 (백엔드 구현 후 연결 예정)

--- jumkumjoong_frontend/src/AppRoutes.tsx
+++ jumkumjoong_frontend/src/AppRoutes.tsx
@@ -22,6 +22,7 @@ import TransactionsPage from "./pages/TransactionPage/TransactionPage";
 import FavoritePage from "./pages/FavoritesPage/FavoritesPage";
 import MyPostsPage from "./pages/userPage/myPostPage";
 import { getUserInfo } from "./api/users";
+import ReviewRegisterPage from "./pages/ReviewPage/ReviewRegisterPage";
 
 const AppRoutes = () => {
   const navigate = useNavigate();
@@ -65,6 +66,7 @@ const AppRoutes = () => {
       <Route path="/user/login" element={<LoginPage />} />
       <Route path="/mypage" element={<MyPage />} />
       <Route path="/reviews" element={<ReviewListPage />} />
+      <Route path="/reviews/register" element={<ReviewRegisterPage />} />
       <Route path="/transactions" element={<TransactionsPage />} />
       <Route path="/favorites" element={<FavoritePage />} />
       <Route path="/my-posts" element={<MyPostsPage />} />
