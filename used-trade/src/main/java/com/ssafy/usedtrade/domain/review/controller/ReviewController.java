2a83d30898ef007b65f9fb695c1a529d0e7de1d1
고성태 <v12rhtjdxo@naver.com>
2025-05-07 11:08:33 +0900

    fix: review slice 적용

--- used-trade/src/main/java/com/ssafy/usedtrade/domain/review/controller/ReviewController.java
+++ used-trade/src/main/java/com/ssafy/usedtrade/domain/review/controller/ReviewController.java
@@ -5,13 +5,16 @@ import com.ssafy.usedtrade.domain.auth.entity.SecurityMemberDetails;
 import com.ssafy.usedtrade.domain.review.dto.ReviewRequest;
 import com.ssafy.usedtrade.domain.review.dto.ReviewResponse;
 import com.ssafy.usedtrade.domain.review.service.ReviewService;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Slice;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,8 +33,11 @@ public class ReviewController {
 
     @GetMapping
     public Api<Slice<ReviewResponse>> findAllReview(
-            @AuthenticationPrincipal SecurityMemberDetails memberDetails) {
-        return Api.OK(reviewService.findAllReview(memberDetails.getId()));
+            @AuthenticationPrincipal SecurityMemberDetails memberDetails,
+            @RequestParam(value = "created-at", required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            LocalDateTime createdAt) {
+        return Api.OK(reviewService.findAllReview(memberDetails.getId(), createdAt));
     }
 
     @GetMapping("/stars")
