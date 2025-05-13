ec13f079381400e2920075772700f96d8e2b44b6
노진원 <wlsdnjs1022@naver.com>
2025-05-13 12:54:34 +0900

    웹소켓 프로토콜 변경

--- jumkumjoong_frontend/src/App.tsx
+++ jumkumjoong_frontend/src/App.tsx
@@ -6,7 +6,7 @@ import { ChatServiceProvider } from "./poviders/ChatServiceProvider";
 
 function App() {
   return (
-    <ChatServiceProvider websocketUrl="https://gamjeongjung.co.kr/api/ws">
+    <ChatServiceProvider websocketUrl="wss://gamjeongjung.co.kr/api/ws">
       <ChatProvider>
         <Router>
           <AppRoutes />
