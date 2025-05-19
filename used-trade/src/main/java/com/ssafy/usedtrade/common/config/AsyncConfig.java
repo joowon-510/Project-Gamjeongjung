165bfac5b5d62491a026896beced0b85ebec8ae2
Ruth <v12rhtjdxo@naver.com>
2025-05-19 11:06:55 +0900

    fix: config 수정
    1. 쓰레드 수 down
    2. shutdown 추가

--- used-trade/src/main/java/com/ssafy/usedtrade/common/config/AsyncConfig.java
+++ used-trade/src/main/java/com/ssafy/usedtrade/common/config/AsyncConfig.java
@@ -1,30 +1,29 @@
 package com.ssafy.usedtrade.common.config;
 
-import java.util.concurrent.Executor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class AsyncConfig {
-    @Bean(name = "readStreamExecutor")
-    public Executor readStreamExecutor() {
+    @Bean(name = "readStreamExecutor", destroyMethod = "shutdown")
+    public ThreadPoolTaskExecutor readStreamExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(1);     // 최소 스레드 수
-        executor.setMaxPoolSize(2);      // 최대 스레드 수
+        executor.setMaxPoolSize(1);      // 최대 스레드 수
         executor.setQueueCapacity(100);  // 대기 큐 용량
         executor.setThreadNamePrefix("read-stream-");
         executor.initialize();
         return executor;
     }
 
-    @Bean(name = "chattingStreamExecutor")
-    public Executor chattingStreamExecutor() {
+    @Bean(name = "chatStreamExecutor", destroyMethod = "shutdown")
+    public ThreadPoolTaskExecutor chatStreamExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(1);     // 최소 스레드 수
-        executor.setMaxPoolSize(2);      // 최대 스레드 수
+        executor.setMaxPoolSize(1);      // 최대 스레드 수
         executor.setQueueCapacity(100);  // 대기 큐 용량
-        executor.setThreadNamePrefix("chatting-stream-");
+        executor.setThreadNamePrefix("chat-stream-");
         executor.initialize();
         return executor;
     }
